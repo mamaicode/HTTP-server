2325f689b7043100ab3c6d74ed7225916cd9da64
scirin <irakokakola@gmail.com>
2022-04-11 04:23:48 +0400

    Splitting code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,68 @@
+use http::request::Request;
+use server::Server;
+
 fn main() 
-{    // Passing IP and Port by String, server listening localhost
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
-
-    dbg!(&string);
-    dbg!(string_slice);
-
-    let server = Server::new("127.0.0.1:8080");       
+{      
+    // Passing IP and Port by String, server listening localhost
+    let server = Server::new("127.0.0.1:8080".to_string());       
     server.run();
 }
 
-
-struct Server
+mod server
 {
-    addr: String, 
-}
-
-// Implementation block for struct
-impl Server
-{
-    // Writing associative function
-    fn new(addr: String) -> Self                           
+    pub struct Server
     {
-        Self
+        addr: String, 
+    }
+
+    // Implementation block for struct
+    impl Server
+    {
+    // Writing associative function
+        pub fn new(addr: String) -> Self                           
         {
-            addr
-        }
-    }   
+            Self { addr } 
+        }      
     
     // Implementing the run method
-    fn run(self)
-    {
+        pub fn run(self)
+        {   
+            println!("Listening on {}", self.addr)
+        }
 
     }
 
+}
+
+
+mod http
+{   
+    pub mod request
+    {
+        use super::method:Method;
+        // Modeling the data we work with, handling HTTP requests and returning HTTP responces 
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+        pub struct Request
+        {   path: String,
+            // Absence of a value in a type safe way without a fear of no pointer exceptions <String>
+            query_string: Option<String>,                   
+            method: Method,
+        }
+    }
+
+    pub mod method
+    {
+        pub enum Method
+        {
+            GET,
+            HEAD,
+            POST,
+            PUT,
+            DELETE,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }    
 }
