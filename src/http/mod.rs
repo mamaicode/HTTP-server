f01f28bfc79bd9a4b56008b12b57f708fadf4ac9
scirin <irakokakola@gmail.com>
2022-04-23 16:09:07 +0400

    Adding HTTP responses

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -4,7 +4,9 @@ pub use method::Method;
 // Exposing parse error
 pub use request::ParseError;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
 pub mod request;
 pub mod method;
 pub mod query_string;
+pub mod response;
---
+++ src/http/response.rs
@@ -0,0 +1,21 @@
+pub enum StatusCode{}
+
+// Representing HTTP response 
+// This struct stores the status code and the body
+pub struct Response
+{ 
+     // Stores the HTTP status code and the body
+    status_code: StatusCode,
+    // In case the body doesn't have a response we wrap an option around the string
+    body: Option<String>,
+
+}
+
+// Constructor for the response
+impl Response
+{
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+                                                                    // Creating a new response
+                                                                    Response{status_code,body}
+                                                                 }
+}
