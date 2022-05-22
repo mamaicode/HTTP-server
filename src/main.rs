eefbf9c849b1a8a53e28a09d81cb523d69e09dbb
scirin <irakokakola@gmail.com>
2022-05-22 21:21:54 +0400

    Serving HTML files

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@
 
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() 
-{      
+{   
+    // Using the code on someone elses computer & passing format macro
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    // Read the public path variable from the environment when the process starts up 
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);  
+    println!("public path: {}", public_path);
     // Passing IP and Port by String, server listening localhost
     let server = Server::new("127.0.0.1:8080".to_string());       
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path)); 
 }
 
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,7 +1,27 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler
+{
+    public_path: String
+}
+
+impl WebsiteHandler
+{
+    pub fn new(public_path: String) -> Self{
+                                             Self { public_path }
+                                           }
+    // Method to route, returning an option in case file does not exist
+    fn read_file(&self, file_path: &str) -> Option<String>{
+                                                            // Reading the public path and append the file path to it
+                                                            let path = format!("{}/{}", self.public_path, file_path);
+                                                            // Read file to string
+                                                            fs::read_to_string(path).ok()                                                                                                      
+                                                          } 
+
+
+}
 
 impl Handler for WebsiteHandler
 {
@@ -9,8 +29,8 @@ impl Handler for WebsiteHandler
                                                                   match request.method(){
                                                                                             // Handling GET requests
                                                                                             Method::GET => match request.path(){
-                                                                                                                                  "/" => Response::new(StatusCode::Ok, Some("<h1>OK</h1>".to_string())),
-                                                                                                                                  "/user" => Response::new(StatusCode::Ok, Some("<h1>Welcome User</h1>".to_string())),
+                                                                                                                                  "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                                                                                                                                  "/user" => Response::new(StatusCode::Ok, self.read_file("user.html")),
                                                                                                                                   _ => Response::new(StatusCode::NotFound, None),      
                                                                                                                                }
 
